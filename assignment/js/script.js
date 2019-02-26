3d3ad2faa9999c3d0e633e08f6ec666ee71545e5
jesperingels <jesper.ingels@gmail.com>
2019-02-26 14:28:40 +0100

    Add battery lvl

--- assignment/js/script.js
+++ assignment/js/script.js
@@ -11,13 +11,14 @@ document.addEventListener('mousemove', function (e) {
     document.documentElement.style.setProperty('--pos-mouse-y', y + 'deg');
 });
 
-var input = document.querySelector('input[class="slider"]');
+const input = document.querySelector('input[class="slider"]');
 
 
 input.addEventListener('input', dimming);
 
 function dimming() {
-    let inputValueHigh, inputValueLow, inputValueMedium;
+    let inputValueHigh, inputValueLow, inputValueMedium, inputValue;
+    inputValue = input.value;
     inputValueMedium = (input.value / 2) + 25;
     inputValueLow = input.value / 2 + 10;
     inputValueHigh = (input.value / 2) + 50;
@@ -25,6 +26,52 @@ function dimming() {
     document.documentElement.style.setProperty('--bg', inputValueMedium + '%');
     document.documentElement.style.setProperty('--bg-light', inputValueHigh + '%');
     document.documentElement.style.setProperty('--bg-dark', inputValueLow + '%');
+    document.documentElement.style.setProperty('--window-height', inputValue + '%');
+}
+
+const inputBat = document.getElementById('battery');
+inputBat.addEventListener('input', batteryLvl);
+
+function batteryLvl() {
+
+    if(inputBat.value <= 75) {
+        document.querySelector('.beamer__beam-right').setAttribute('id','flicker-r');
+        document.querySelector('[datatype="right"]').setAttribute('id','flicker-r')
+
+    } else {
+        document.querySelector('.beamer__beam-right').removeAttribute('id');
+        document.querySelector('[datatype="right"]').removeAttribute('id');
+
+    }
+
+    if(inputBat.value <= 50) {
+        document.querySelector('.beamer__beam-left').setAttribute('id','flicker-l');
+        document.querySelector('[datatype="left"]').setAttribute('id','flicker-l')
+    } else {
+        document.querySelector('.beamer__beam-left').removeAttribute('id');
+        document.querySelector('[datatype="left"]').removeAttribute('id');
+
+    }
+
+
+    if(inputBat.value <= 30) {
+        document.querySelector('.beamer__beam-right').style.display = 'none';
+        document.querySelector('[datatype="right"]').style.display = 'none';
+
+    } else {
+        document.querySelector('.beamer__beam-right').style.display = 'block';
+        document.querySelector('[datatype="right"]').style.display = 'block';
+    }
+
+    if (inputBat.value <= 15 ){
+        document.querySelector('.beamer__beam-left').style.display = 'none';
+        document.querySelector('[datatype="left"]').style.display = 'none';
+    } else {
+        document.querySelector('.beamer__beam-left').style.display = 'block';
+        document.querySelector('[datatype="left"]').style.display = 'block';
+
+    }
+
 }
 
 dimming();
